d6c587db6a7c64f393e334ff2e4118b946125101
isekar0 <martinhanna002@gmail.com>
2025-02-20 12:18:48 +0400

    Update main.py
    Revised the code.
    Changed it to follow OOP paradigm.
    Now allows for board sizes greater than 3.

--- main.py
+++ main.py
@@ -1,140 +1,313 @@
 #include <bits/stdc++.h>
 #define endl '\n'
-#define N 3 // Does not work for grids larger than 3x3
-#define SIZE N * N
 using namespace std;
 
-typedef struct Action{
-    int player;
-    int index;
-} Action;
+typedef vector<vector<char>> square; 
 
+class Board {
+    private:
+        int N = 3;
+        int SIZE = N * N;
+        char player;
+        square grid;
+        vector<int> valid_moves;
+        bool is_terminal;
+        
+    public:
+        char winner;
 
-Action getOppositePlayer(Action action) {
-    action.player = -action.player;
-    return action;
-}
-
-const vector<int> getValidMoves(const vector<int> grid) {
-    vector<int> valid_moves(SIZE, 0);
-    for (int i = 0; i < SIZE; i++) {
-        if (grid[i] == 0) {
-            valid_moves[i] = 1;
+    Board(const int user_n) {
+        if (user_n > 2 && user_n < 10) {
+            initGridSize(user_n);
         }
+        grid = initGrid();
+        player = initRandPlayer();
+        valid_moves = vector<int>(SIZE, 1);
+        is_terminal = false;
+        winner = 'n';
     }
 
-    return valid_moves;
-}
+    int getSize() {
+        return SIZE;
+    }
 
-void getAction(vector<int> &grid, Action &action, const vector<int> valid_moves) {
-    bool played_succesfully = false;
-    while (played_succesfully == false) {
-        do {
-            cout << "Player #" << action.player << ", please enter a valid move (index 0 - 8): ";
-            cin >> action.index;
-            cout << endl;
-        } while (action.index < 0 || action.index > 8);
+    char getPlayer() {
+        return player;
+    }
 
-        // cout << "You chose to play in tile " << action.index << endl;
-        if (valid_moves[action.index] == 1) {
-            // cout << "played once" << endl;
-            // cout << action.index << endl;
-            // cout << grid << endl;
+    square getGrid() {
+        return grid;
+    }
+    
+    vector<int> getValidMoves() {
+        return valid_moves;
+    }
+    
+    bool getIsTerminal() {
+        return is_terminal;
+    }
+    
+    void initGridSize(const int user_n) {
+        N = user_n; 
+        SIZE = N * N;
+    }
+    
+    square initGrid() {
+        square board = square(N, vector<char>(N, '.'));
+        return board;
+    }
 
-            grid[action.index] = action.player;
-            played_succesfully = true;
-            break;
+    // Initialize starting player at random, x will be human and o will be computer.
+    char initRandPlayer() {
+        char player_ = '\0';
+        int random_player_int = rand() % 2;
+        switch (random_player_int) {
+            case 0: player_ = 'x'; break;
+            case 1: player_ = 'o'; break;
+            default: initRandPlayer();
+        }
+
+        return player_;
+    }
+
+    void getOppositePlayer() {
+        if (player == 'x') {
+            player = 'o';
+            return;
+        }
+        else if (player == 'o') {
+            player = 'x';
+            return;
         }
         else {
-            cout << "Enter valid action!" << endl;
+            cerr << "Invalid player value!" << endl;
+            exit(EXIT_FAILURE);
         }
     }
-}
-int getTerminal(const int player, const vector<int> grid, const vector<int> valid_moves) {
-    // Check for draw
-    int counter = 0;
-    for (int i = 0; i < SIZE; i++) {
-        if (valid_moves[i] == 0) {
-            counter += 1;
-        }
-        if (counter == SIZE) {
-            return -2;
-        }
-    }
-    // Check n in row
-    for (int i = 0; i < SIZE - 2; i++) {
-        if ((grid[i] != 0 && grid[i + 1] != 0 && grid[i + 2] != 0) && (grid[i] == grid[i + 1] && grid[i + 1] == grid[i + 2]))
-            return player;
-    }
-    // Check n in column
-    for (int i = 0; i < N; i++) {
-        if ((grid[i] != 0 && grid[i + N] != 0 && grid[i + 2 * N] != 0) && (grid[i] == grid[i + N] && grid[i + N] == grid[i + 2 * N])) {
-            return player;
-        }
-    }
-    // Check n in diag
-    if ((grid[0] != 0 && grid[N + 1] != 0 && grid[2 * N + 2] != 0) && (grid[0] == grid[N + 1] && grid[N + 1] == grid[2 * N + 2])) {
-        return player;
-    }
-    // Check n in diag opposite
-    if ((grid[N - 1] != 0 && grid[2 * N - 2] != 0 && grid[3 * N - 3] != 0) && (grid[N - 1] == grid[2 * N - 2] && grid[2 * N - 2] == grid[3 * N - 3])) {
-        return player;
-    }
-    else {
-        return 0;
-        }
-}
 
-void printGrid(vector<int> grid) {
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < N; j++) {
-            cout << grid[N * i + j] << " | ";
+    void printGrid() {
+        vector<string> seperater_line(11 + 4 * (N - 3), "-"); // Found this to make nice lines
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                cout << grid.at(i).at(j) << " | ";
+            }
+            cout << '\n';
+            for (int j = 0; j < seperater_line.size(); j++) {
+                cout << seperater_line.at(i);
+            }
+            cout << '\n';    
         }
-        cout << "\n-----------" << endl;
     }
-    // for (int i = 0; i < SIZE; i++) 
-    //     cout << grid[i] << " | " << endl;
-}
 
-void printGrid(vector<int> grid, vector<int> valid_moves) {
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < N; j++) {
-            cout << grid[N * i + j] << " | ";
+    void updateValidMoves() {
+        vector<int> new_valid_moves(SIZE, 0);
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (grid.at(i).at(j) == '.') {
+                    new_valid_moves[N * i + j] = 1;
+                }
+            }
         }
-        cout << "\n-----------" << endl;
+        valid_moves = new_valid_moves;
     }
-    cout << endl;
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < N; j++) {
-            cout << valid_moves[N * i + j] << " | ";
+
+    void printValidActions() {
+        cout << "Available Moves (by index): ";
+        for (int i = 0; i < SIZE; i++) {
+            if (valid_moves[i] == 1) {
+                cout << i << ", ";
+            }
         }
-        cout << "\n-----------" << endl;
+        cout << endl;
+    }
+
+    int getHumanAction() {
+        int player_action = -1;
+        do {
+            cout << "Choose available index to play in [0 - " << SIZE - 1 << "]: "; 
+            cin >> player_action;
+        } while (valid_moves[player_action] != 1);
+
+        return player_action;
+    }
+
+    int getComputerAction() {
+        int computer_action = -1;
+        vector<int> available_moves = vector<int>(0,0);
+        
+        for (int i = 0; i < SIZE; i++) {
+            
+            if (valid_moves.at(i) == 1) {
+                available_moves.push_back(i);
+            }
+        }
+        computer_action = rand() % available_moves.size();
+
+        return available_moves[computer_action];
+    }
+
+    void updateIsTerminal() {
+        if (checkWin()) {
+            is_terminal = true;
+            return;
+        }
+        
+        bool emptyFound = false;
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (grid[i][j] == '.') {
+                    emptyFound = true;
+                    break;
+                }
+            }
+            if (emptyFound) break;
+        }
+        
+        if (!emptyFound) {
+            is_terminal = true;
+            winner = 'd'; 
+        } else {
+            is_terminal = false;
+        }
+    }
+    
+
+    bool checkWin() {
+        for (int i = 0; i < N; i++) {
+            char first = grid[i][0];
+            if (first == '.') continue; 
+            bool win = true;
+            for (int j = 1; j < N; j++) {
+                if (grid[i][j] != first) {
+                    win = false;
+                    break;
+                }
+            }
+            if (win) {
+                winner = first;
+                return true;
+            }
+        }
+        
+        for (int j = 0; j < N; j++) {
+            char first = grid[0][j];
+            if (first == '.') continue; 
+            bool win = true;
+            for (int i = 1; i < N; i++) {
+                if (grid[i][j] != first) {
+                    win = false;
+                    break;
+                }
+            }
+            if (win) {
+                winner = first;
+                return true;
+            }
+        }
+      
+        char first = grid[0][0];
+        if (first != '.') {
+            bool win = true;
+            for (int i = 1; i < N; i++) {
+                if (grid[i][i] != first) {
+                    win = false;
+                    break;
+                }
+            }
+            if (win) {
+                winner = first;
+                return true;
+            }
+        }
+        
+        first = grid[0][N - 1];
+        if (first != '.') {
+            bool win = true;
+            for (int i = 1; i < N; i++) {
+                if (grid[i][N - 1 - i] != first) {
+                    win = false;
+                    break;
+                }
+            }
+            if (win) {
+                winner = first;
+                return true;
+            }
+        }
+        
+        return false;
+    }
+    
+    void playAction(const int action) {
+        int column = 0, row = 0;
+        column = action % N;
+        row = action / N;
+        grid.at(row).at(column) = player;
+    }
+};
+
+void game() {
+    int user_n = 3;
+    char play_again = '\0';
+    cout << "Let's play Tic Tac Toe!" << endl;
+    cout << "What size board would you like to play in: ";
+    cin >> user_n;
+    if (user_n < 2 || user_n > 10) {
+        cout << "Please be reasonable! Board size will default to 3!" << endl;
+        user_n = 3;
+    }
+    
+    Board tictactoe(user_n);
+    
+    while (!tictactoe.getIsTerminal()) { 
+        tictactoe.updateValidMoves();
+        
+        int action = -1;
+        if (tictactoe.getPlayer() == 'x') {
+            cout << "Your turn!" << endl;
+            tictactoe.printGrid();
+            tictactoe.printValidActions();
+            action = tictactoe.getHumanAction();
+        } else {
+            action = tictactoe.getComputerAction();
+        }
+        
+        tictactoe.playAction(action);
+        tictactoe.updateIsTerminal();
+        
+        if (!tictactoe.getIsTerminal())
+            tictactoe.getOppositePlayer();
+    }
+    tictactoe.printGrid();
+    
+    if (tictactoe.winner == 'd') {
+        cout << "It's a draw!" << endl;
+    } else {
+        if (tictactoe.winner == 'x') {
+            cout << "You win!" << endl;
+        } else if (tictactoe.winner == 'o') {
+            cout << "Computer wins!" << endl;
+        }
+    }
+    
+    while (play_again != 'y' && play_again != 'n') {
+        cout << "Play again? (y/n): ";
+        cin >> play_again;
+        if (play_again != 'y' && play_again != 'n') {
+            cout << "Please enter [y/n]..." << endl;
+        }
+    }
+    
+    if (play_again == 'y') {
+        game();  
+    } else {
+        cout << "Bye bye!" << endl;
     }
 }
 
 int main(void) {
-    int player = 1, winner = 0;
-    bool played_succesfully = false;
-    vector<int> grid(SIZE, 0), valid_moves(SIZE, 0);
-    Action action = {player, -1};
-    
-    while (winner == 0) {
-        valid_moves = getValidMoves(grid);
-        printGrid(grid);
-
-        cout << "***************** NEXT TURN **********************" << endl;
-        getAction(grid, action, valid_moves); 
-        action = getOppositePlayer(action);
-            
-        winner = getTerminal(player, grid, valid_moves);
-    } 
-    if (winner == 1 || winner == -1) {
-        cout << "Winner is Player #" << winner << "!\n" << endl;
-    }
-    else {
-        cout << "Draw!" << endl;
-    }
-
+    srand(time(0));
+    game();
     return 0;
 }
 
